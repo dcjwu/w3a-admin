a328b4a5172444a47524c6632e5b6bac0f0af76f
AK <kkucenko.aleksejs@gmail.com>
2022-07-04 22:04:11 +0300

    Chore | Remove preact and bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,21 +1,3 @@
 /** @type {import("next").NextConfig} */
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")( // eslint-disable-line @typescript-eslint/no-var-requires
-   { enabled: process.env.ANALYZE === "true" })
-
-module.exports = withBundleAnalyzer({
-   reactStrictMode: true,
-   webpack: (config, { // eslint-disable-line @typescript-eslint/explicit-function-return-type
-      dev,
-      isServer
-   }) => {
-      if (!dev && !isServer) {
-         Object.assign(config.resolve.alias, {
-            react: "preact/compat",
-            "react-dom/test-utils": "preact/test-utils",
-            "react-dom": "preact/compat"
-         })
-      }
-      return config
-   }
-})
+module.exports = { reactStrictMode: true }
