4e4f63afb772e5aa61618379ee189a2341b07a06
AK <kkucenko.aleksejs@gmail.com>
2022-07-04 22:01:51 +0300

    Revert "Chore | Update config to new preact"
    This reverts commit e35b9bf9d8e13b9b2ce5020a07dcf1b9fc759b09.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import("next").NextConfig} */
 
-const withPreact = require("next-plugin-preact")  // eslint-disable-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require("@next/bundle-analyzer")( // eslint-disable-line @typescript-eslint/no-var-requires
+   { enabled: process.env.ANALYZE === "true" })
 
-
-module.exports = withPreact({ reactStrictMode: true })
+module.exports = withBundleAnalyzer({
+   reactStrictMode: true,
+   webpack: (config, { // eslint-disable-line @typescript-eslint/explicit-function-return-type
+      dev,
+      isServer
+   }) => {
+      if (!dev && !isServer) {
+         Object.assign(config.resolve.alias, {
+            react: "preact/compat",
+            "react-dom/test-utils": "preact/test-utils",
+            "react-dom": "preact/compat"
+         })
+      }
+      return config
+   }
+})
