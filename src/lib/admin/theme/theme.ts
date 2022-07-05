6c190266194fb37b50ab48b3e4d53daea81660b6
AK <kkucenko.aleksejs@gmail.com>
2022-07-06 01:16:53 +0300

    Chore | Add new color to theme

--- src/lib/admin/theme/theme.ts
+++ src/lib/admin/theme/theme.ts
@@ -4,7 +4,8 @@ export const theme = createTheme({
    palette: {
       primary: { main: "#264653", },
       secondary: { main: "#E76F51", },
-      background: { default: "#f5f5f5", },
+      info: { main: "#2A9D8F", },
+      background: { default: "#F5F5F5", },
       text: { primary: "#101010", },
    },
 })
