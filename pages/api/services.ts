7d5865cc34c76a3a9782209bbba98baac22ada42
AK <kkucenko.aleksejs@gmail.com>
2022-07-13 16:30:26 +0300

    Chore | Update prisma schema and run migration

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -7,10 +7,21 @@ import { prisma } from "@lib/prisma"
 
 import type { NextApiRequest, NextApiResponse } from "next"
 
-const schema = Joi.object({
+const PostServiceDto = Joi.object({
    name: Joi.string().required(),
    description: Joi.string().required()
 })
+
+const PatchServiceDto = Joi.object({
+   id: Joi.string().uuid().required(),
+   fieldName: Joi.string().required(),
+   newValue: Joi.string().required()
+})
+
+enum ServiceFieldName {
+   Name = "name",
+   Description = "description"
+}
 
 const router = createRouter<NextApiRequest, NextApiResponse>()
 
@@ -28,7 +39,7 @@ router
       }
    })
    
-   .post(validationMiddleware({ body: schema }), async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+   .post(validationMiddleware({ body: PostServiceDto }), async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
       try {
          const service = req.body
 
@@ -39,7 +50,39 @@ router
             },
          })
 
-         return res.status(200).json({ message: "Service created" })
+         return res.status(201).json({ message: "Service created" })
+
+      } catch (err) {
+         console.error(err.message)
+         return res.status(400).json({ message: err.message })
+      }
+   })
+   
+   .patch(validationMiddleware({ body: PatchServiceDto }), async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+      try {
+         const service = req.body
+
+         if (Object.values(ServiceFieldName).includes(service.fieldName)) {
+
+            if (service.fieldName === ServiceFieldName.Name) {
+               await prisma.service.update({
+                  where: { id: service.id },
+                  data: { name: service.newValue }
+               })
+            }
+
+            if (service.fieldName === ServiceFieldName.Description) {
+               await prisma.service.update({
+                  where: { id: service.id },
+                  data: { description: service.newValue }
+               })
+            }
+
+            return res.status(200).json({ message: `${service.fieldName} updated successfully` })
+
+         } else {
+            return res.status(422).json({ message: `${service.fieldName} is not recognized` })
+         }
 
       } catch (err) {
          console.error(err.message)
@@ -53,6 +96,6 @@ export default router.handler({
       res.status(500).end("Internal server error")
    },
    onNoMatch: (req, res) => {
-      res.status(404).end("Not found")
+      res.status(405).end("Method not allowed")
    },
 })
---
+++ prisma/migrations/20220713121320_initial_migration/migration.sql
@@ -0,0 +1,141 @@
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresToken" TEXT,
+    "accessToken" TEXT,
+    "expiresAt" INTEGER,
+    "tokenType" TEXT,
+    "scope" TEXT,
+    "idToken" TEXT,
+    "sessionState" TEXT,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "verificationTokens" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "topic" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "ipAddress" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "services" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "services_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "partners" (
+    "id" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "link" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "partners_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "stack" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "link" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "stack_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "portfolio" (
+    "id" TEXT NOT NULL,
+    "projectName" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "portfolio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teamMembers" (
+    "id" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "socialMediaLinks" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "teamMembers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_providerAccountId_key" ON "accounts"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_sessionToken_key" ON "sessions"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationTokens_token_key" ON "verificationTokens"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationTokens_identifier_token_key" ON "verificationTokens"("identifier", "token");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
