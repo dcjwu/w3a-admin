8bd1ac323b66fa2ace0de84a73ec53c871b1f87d
AK <kkucenko.aleksejs@gmail.com>
2022-08-02 21:09:41 +0300

    Chore | Add maintenance mode through .env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 /** @type {import("next").NextConfig} */
 
-module.exports = { reactStrictMode: true, images: { domains: ["public-web3app.s3.eu-north-1.amazonaws.com"] } }
+module.exports = {
+   reactStrictMode: true,
+   images: { domains: ["public-web3app.s3.eu-north-1.amazonaws.com"] },
+   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+   redirects() {
+      return [
+         process.env.MAINTENANCE_MODE === "1"
+            ? { source: "/", destination: "/maintenance", permanent: false }
+            : { source: "/maintenance", destination: "/", permanent: false },
+      ].filter(Boolean)
+   }
+}
